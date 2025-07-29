24cd0d64ec8481a3418678f8d1903beadf0bdcbe
SHRESTH <Shresthkrdos@gmail.com>
2025-07-29 17:33:06 +0530

    Added Increment function in Reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Counter from "./components/Counter";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 function App() {
   return (
-    <div></div>
-  )
+    <Provider store={store}>
+      <div>
+        <Counter />
+      </div>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment } from "../features/counter/counterSlice";
+
+function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <div className="bg-yellow-100 w-full h-screen flex items-center justify-center">
+      <div className="bg-yellow-100 w-full h-auto flex items-center justify-center">
+        <div className="bg-white w-1/5 p-10 rounded shadow-md ">
+          <div className="flex items-center justify-center gap-3">
+            <h1 className="text-2xl text-black ">
+              <strong>Counter: </strong>
+            </h1>
+            <p className="flex items-center justify-center  text-2xl text-black">
+              {count}
+            </p>
+          </div>
+          <div className="flex items-center justify-center mt-10">
+            <button onClick={()=>dispatch(increment())} className="bg-yellow-200 hover:bg-amber-300 px-4 py-1 rounded-2xl">
+              Increment
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
